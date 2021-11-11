a7e25f0e21edff41a3311f7e92760f98ffaa6aff
bashforger <bash.forger@gmail.com>
2021-11-11 19:29:28 +0500

    Added Deployed contract address

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -4,3 +4,5 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 dotenv.config();
 const web3 = createAlchemyWeb3(process.env.API_URL)
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json") // ABI
+const contractAddress = "0xe62af8d35e856ef360cab9bcfea7b4c15f8f0666"
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
